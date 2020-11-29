0553ae6d31e8aa62fd5b69bed193ec468fe5991d
Asmerom <miklelovemam@gmail.com>
2020-11-29 23:32:13 +0100

    Second commit

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ if(theme == null){
   setTheme(theme)
 }
 
-
 let themeDots = document.getElementsByClassName('theme-dot');
 
 for (var i = 0; themeDots.length > i; i++){
@@ -17,21 +16,18 @@ for (var i = 0; themeDots.length > i; i++){
   })
 }
 function setTheme(mode){
-  if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
-
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-  }
+        if(mode == 'light'){
+          document.getElementById('theme-style').href = 'default.css'
+        }
+        if(mode == 'blue'){
+          document.getElementById('theme-style').href = 'blue.css'
+        }
+        if(mode == 'green'){
+          document.getElementById('theme-style').href = 'green.css'
+        }
+        if(mode == 'purple'){
+          document.getElementById('theme-style').href = 'purple.css'
+        }
 
 localStorage.setItem('theme', mode)
 
